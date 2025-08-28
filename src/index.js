f05111e95bbca189ac610db2470c5b14047be90a
souravmalik8499 <souravmalik8499@gmail.com>
2025-08-28 23:54:53 +0530

    Updated React Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
+import Signup from './landing_page/signup/Signup';
+import AboutPage from './landing_page/about/AboutPage';
+import ProductPage from './landing_page/Product/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import Navbar from './landing_page/Navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HomePage/>  
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/Product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+    </Routes>
+    <footer />
+  </BrowserRouter>
 );
