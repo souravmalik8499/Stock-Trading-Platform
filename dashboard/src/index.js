8a8a30fe4e6af5ba1c772136977131480fd960a7
souravmalik8499 <souravmalik8499@gmail.com>
2025-09-18 21:32:01 +0530

    Added backend and dashboard folders

---
+++ dashboard/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './index.css';
+import HomePage from './landing_page/home/HomePage';
+import Signup from './landing_page/signup/Signup';
+import AboutPage from './landing_page/about/AboutPage';
+import ProductPage from './landing_page/Product/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import Navbar from './landing_page/Navbar';
+import NotFound from './landing_page/NotFound';
+import "font-awesome/css/font-awesome.min.css";
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/Product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+    <footer />
+  </BrowserRouter>
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+ c5c16a9 (implement dashboard)
+);
---
+++ dashboard/src/landing_page/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "./Hero";
+import Team from "./Team";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import PricingPage from "../pricing/PricingPage";
+
+function AboutPage() {
+    return (
+        <>
+        <Hero />
+        <Team />
+        <Footer />
+        </>
+    );
+}
+export default AboutPage;
